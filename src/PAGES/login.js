32be67c88178e9a180093077465846dc01bed359
bsdimen <nimene505@gmail.com>
2024-07-05 22:57:17 +0100

    05/07/2024 -p-2

--- src/PAGES/login.js
+++ src/PAGES/login.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import "../CSS/main.css";
 
 export default function Login ()   {
 
         return (
-            <div className='center-box login'>
-                <form>
-                    <input />
-                    <input />
+            <div className='login center-box'>
+                <form className='login-form' >
+                    <input className='login-input' type='email'/>
+                    <input className='login-input' type='password'/>
                     <button>Log in</button>
                 </form>
             </div>
