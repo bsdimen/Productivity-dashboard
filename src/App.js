bed3ef1c11d9ae46a44f16a15f307ecd884a7546
bsdimen <nimene505@gmail.com>
2024-07-05 20:50:19 +0100

    05/07/2024

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {RouterProvider,Route,createBrowserRouter,createRoutesFromElements} from 'react-router-dom';
+import Login from './PAGES/login';
+import RootLayout from './COMPONENTS/RootLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Login />}>
+       <Route index element={<Login />}/>
+    </Route>
+    )
+)
+        
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/PAGES/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Login ()   {
+
+        return (
+            <div className='center-box login'>
+                <form>
+                    <input />
+                    <input />
+                    <button>Log in</button>
+                </form>
+            </div>
+        );
+}
+
+
